d85b21394129576a51fd3f603735624c3274b588
LeoWang <LeoWang@inslabs.org>
2020-05-08 17:21:28 +0800

    [MOD]添加通用项目使用的测试类

---
+++ skylake/src/main/java/com/upuphub/lake/skylake/benchmark/Test.java
@@ -0,0 +1,9 @@
+package com.upuphub.lake.skylake.benchmark;
+
+public class Test {
+    public static void main(String[] args) {
+        String name = "LeoWang";
+        // \u000dname="Show LeoWang"; 
+        System.out.println(name);
+    }
+}
