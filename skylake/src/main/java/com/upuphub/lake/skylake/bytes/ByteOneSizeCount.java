372093a0f7acaf16fb727dfcab554e947eb3f193
LeoWang <LeoWang@Inslabs.org>
2020-07-06 17:35:03 +0800

    [ADD]二进制中的1的个数计数统计

---
+++ skylake/src/main/java/com/upuphub/lake/skylake/bytes/ByteOneSizeCount.java
@@ -0,0 +1,18 @@
+package com.upuphub.lake.skylake.bytes;
+
+public class ByteOneSizeCount {
+    public static void main(String[] args) {
+        System.out.println(getByteOneSizeCount(new Long(0b100010001000111)));
+        System.out.println(getByteOneSizeCount(new Long(0b100010001100111)));
+    }
+
+    private static int getByteOneSizeCount(long num){
+        long val = num;
+        int ans = 0;
+        while (val != 0) {
+            ++ans;
+            val = val & (val-1);
+        }
+        return ans;
+    }
+}
